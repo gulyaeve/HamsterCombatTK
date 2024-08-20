09fd759a405b42e085b7807e259570a929a03631
Eugene Gulyaev <gulyaev.e@icloud.com>
2024-08-20 16:49:57 +0300

    Version 1.0

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import tkinter
 
+scores = 0
+
+
+def update_score():
+    global scores
+    scores += 1
+    scores_text.configure(text=scores)
+
 
 app = tkinter.Tk()
+
 app.title("My HamsterCombat")
 app.geometry("300x400")
+
+welcome_text = tkinter.Label(app, text="Welcome to My Hamster Combat!")
+welcome_text.pack()
+
+button = tkinter.Button(app, text="TAP", width=25, height=2, command=update_score)
+button.pack()
+
+scores_text = tkinter.Label(app, text=scores)
+scores_text.pack()
 
 
 if __name__ == '__main__':
